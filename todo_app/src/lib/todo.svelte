6fb747bac3d8b864ca36e55c749c00c2d3cf1220
Appz-dk <patrick.fyhn@gmail.com>
2023-10-20 12:39:18 +0200

    Install & setup icons

--- todo_app/src/lib/todo.svelte
+++ todo_app/src/lib/todo.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import type { TodoType } from "./Todos.svelte";
+  import FaRegTrashAlt from 'svelte-icons/fa/FaRegTrashAlt.svelte'
 
   export let todo: TodoType
 </script>
@@ -9,12 +10,16 @@
     <input type="checkbox" bind:checked={todo.done} />
     {todo.text}
   </label>
+  <div class="icon">
+    <FaRegTrashAlt />
+  </div>
 </div>
 
 
 <style>
   .todo-container {
     display: flex;
+    justify-content: space-between;
     gap: 6px;
     font-size: 1.25rem;
   }
@@ -30,4 +35,15 @@
     text-decoration: line-through;
   }
 
+  .icon {
+    display: grid;
+    place-items: center;
+    width: 20px;
+    aspect-ratio: 1;
+    cursor: pointer;
+  }
+
+  .icon:hover {
+    color: rgb(248, 72, 72)
+  }
 </style>
